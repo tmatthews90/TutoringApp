2eae7ac73f9eb4b120e38edb19c689576e40127e
Travis Atwell <dc3king@hotmail.com>
2016-04-05 23:06:29 -0500

    theme changed to have no action bar. schema updated for class name change

--- app/src/main/java/team7/tutoringappv1/landingPage.java
+++ app/src/main/java/team7/tutoringappv1/landingPage.java
@@ -1,10 +1,12 @@
 package team7.tutoringappv1;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -12,6 +14,7 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
         Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
