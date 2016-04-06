28f9cda244826085c05644b3d2cd6053cb7ce141
Travis Atwell <travis.atwell@mavs.uta.edu>
2016-04-05 19:47:25 -0500

    login page added

--- app/src/main/java/team7/tutoringappv1/landingPage.java
+++ app/src/main/java/team7/tutoringappv1/landingPage.java
@@ -2,6 +2,8 @@ package team7.tutoringappv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class landingPage extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class landingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        Button login = (Button) findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_login);
+            }
+        });
     }
 }
