a9bcd1f86d16ff8bf3d0ad168db62ef69531e2d4
Travis Atwell <dc3king@hotmail.com>
2016-04-06 02:32:41 -0500

    protection of internal features; post registration goes to logon page

--- app/src/main/java/team7/tutoringappv1/RegisterActivity.java
+++ app/src/main/java/team7/tutoringappv1/RegisterActivity.java
@@ -3,6 +3,7 @@ package team7.tutoringappv1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,7 +17,7 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent doneIntent = new Intent(view.getContext(), PostLogin.class);
+                Intent doneIntent = new Intent(view.getContext(), LoginActivity.class);
                 startActivityForResult(doneIntent, 0);
                 finish(); //to stop back button from going back to main menu
             }
