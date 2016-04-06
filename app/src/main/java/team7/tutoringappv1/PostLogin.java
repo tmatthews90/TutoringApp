e35ea8b89a428fccacb80928452b7fb9f1316d6f
Travis Atwell <dc3king@hotmail.com>
2016-04-06 01:34:53 -0500

    implemented logout button

--- app/src/main/java/team7/tutoringappv1/PostLogin.java
+++ app/src/main/java/team7/tutoringappv1/PostLogin.java
@@ -1,7 +1,10 @@
 package team7.tutoringappv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PostLogin extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class PostLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_login);
+        Button logout = (Button) findViewById(R.id.logoutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logoutIntent = new Intent(view.getContext(), LoginActivity.class);
+                startActivityForResult(logoutIntent, 0);
+                finish();
+            }
+        });
     }
+
 
 }
