876c587c5600cadd9f2b631128b78655e1b58b1a
Travis Atwell <dc3king@hotmail.com>
2016-04-06 01:15:05 -0500

    added browse,filter,account, and tutors activities

---
+++ app/src/main/java/team7/tutoringappv1/TutorsActivity.java
@@ -0,0 +1,13 @@
+package team7.tutoringappv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutors);
+    }
+}
