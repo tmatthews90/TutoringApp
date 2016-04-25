855e8c99b9f99eae299d6d17172ec81875b9e9c2
Travis Matthews <travis.matthews90@gmail.com>
2016-04-25 18:44:58 -0500

    Added logic to remove Test User in tutor List

--- app/src/main/java/team7/tutoringappv1/TutorsActivity.java
+++ app/src/main/java/team7/tutoringappv1/TutorsActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TutorsActivity extends Activity {
 
     String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+    List<String> tutorNames = new ArrayList<>();
+    String fName;
+    String lName;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,21 @@ public class TutorsActivity extends Activity {
         setContentView(R.layout.activity_tutors);
 
         // builds tutors array of all users in txt file
-        //Tutors tutors = new Tutors(this);
+        Tutors tutors = new Tutors(this);
 
+        tutors.getTutorList().get(0).getFirstName();
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.actvity_listview, mobileArray);
+        for (int i = 0; i < tutors.getTutorList().size(); i++){
+            fName = tutors.getTutorList().get(i).getFirstName();
+            lName = tutors.getTutorList().get(i).getLastName();
+            name = fName + " " + lName;
+
+            if (!name.equals("Test User")){
+                tutorNames.add(name);
+            }
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.actvity_listview, tutorNames);
 
         ListView listView = (ListView) findViewById(R.id.tutorsView);
         listView.setAdapter(adapter);
