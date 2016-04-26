e6372300c743c3d5759b79c94a75661887fcb2da
Travis Matthews <travis.matthews90@gmail.com>
2016-04-26 15:30:05 -0500

    Added more tutor info to list view

--- app/src/main/java/team7/tutoringappv1/TutorsActivity.java
+++ app/src/main/java/team7/tutoringappv1/TutorsActivity.java
@@ -3,6 +3,8 @@ package team7.tutoringappv1;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,6 +17,11 @@ public class TutorsActivity extends Activity {
     String fName;
     String lName;
     String name;
+    String subject;
+    Float rating;
+    int rate;
+    String rateMoneySign;
+    String entry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +31,49 @@ public class TutorsActivity extends Activity {
         // builds tutors array of all users in txt file
         Tutors tutors = new Tutors(this);
 
+
         for (int i = 0; i < tutors.getTutorList().size(); i++){
             if (tutors.getTutorList().get(i).getIsTutor().equals("1")){
                 fName = tutors.getTutorList().get(i).getFirstName();
                 lName = tutors.getTutorList().get(i).getLastName();
                 name = fName + " " + lName;
+                subject = "Test";
+                rating = tutors.getTutorList().get(i).getReviewRate();
+                rate = tutors.getTutorList().get(i).getTutorRate();
 
-                if (!name.equals("Test User")){
-                    tutorNames.add(name);
+                switch(rate) {
+                    case 1:
+                        rateMoneySign = "$";
+                        break;
+                    case 2:
+                        rateMoneySign = "$$";
+                        break;
+                    case 3:
+                        rateMoneySign = "$$$";
+                        break;
+                    default:
+                        rateMoneySign = "$$$";
+                        break;
+                }
+
+                entry = name + "\n   Subject:  " + subject + "\n   Rating:    "+ rating + "\n   Rate:       " + rateMoneySign;
+
+
+                if (!(fName + " " + lName).equals("Test User")){
+                    tutorNames.add(entry);
                 }
             }
         }
-
-        System.out.printf("%d/%d are tutors in system.\n", tutorNames.size(), tutors.getTutorList().size());
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.actvity_listview, tutorNames);
 
         ListView listView = (ListView) findViewById(R.id.tutorsView);
         listView.setAdapter(adapter);
 
+        AdapterView.OnItemClickListener listener =  listView.getOnItemClickListener();
+
+
+
     }
+
 }
