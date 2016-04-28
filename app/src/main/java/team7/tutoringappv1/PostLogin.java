4a786d07fed5a0d00ef800ba6c6320b6b463702e
Travis Matthews <travis.matthews90@gmail.com>
2016-04-28 10:47:04 -0500

    added new title to postlogin screen

--- app/src/main/java/team7/tutoringappv1/PostLogin.java
+++ app/src/main/java/team7/tutoringappv1/PostLogin.java
@@ -1,26 +1,42 @@
 package team7.tutoringappv1;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class PostLogin extends AppCompatActivity {
+
     SQLiteDatabase mydatabase;
+    TextView txtTitle;
+    String usrName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_login);
-        //(R.id.title);
+
+        final SQLiteDatabase mydatabase = openOrCreateDatabase("Users",MODE_PRIVATE,null);
+
+        txtTitle = (TextView) findViewById(R.id.title);
+
+        Cursor fName = mydatabase.rawQuery("Select firstName from userst where loggedIn = '1'",null);
+        Cursor lName = mydatabase.rawQuery("Select lastName from userst where loggedIn = '1'",null);
+        fName.moveToFirst();
+        lName.moveToFirst();
+        usrName = fName.getString(0) + " " + lName.getString(0);
+
+        txtTitle.setText("Welcome, " + usrName + "!");
+
         Button logout = (Button) findViewById(R.id.btnLogout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mydatabase = openOrCreateDatabase("Users",MODE_PRIVATE,null);
                 mydatabase.execSQL("UPDATE userst SET loggedIn = '0' where loggedIn = '1';");
                 Intent logoutIntent = new Intent(view.getContext(), LoginActivity.class);
                 startActivityForResult(logoutIntent, 0);
