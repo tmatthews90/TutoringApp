03c84330a997a103eac1b95c2ed552d35222cd79
Travis Matthews <travis.matthews90@gmail.com>
2016-04-30 11:07:17 -0500

    Added editTextField listeners to register page

--- app/src/main/java/team7/tutoringappv1/RegisterActivity.java
+++ app/src/main/java/team7/tutoringappv1/RegisterActivity.java
@@ -1,12 +1,21 @@
 package team7.tutoringappv1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,23 +35,58 @@ public class RegisterActivity extends AppCompatActivity {
     String tutor = "0";
 
 
+    EditText fieldFName;
+    EditText fieldLName;
+    EditText fieldEmail;
+    EditText fieldPhone;
+    EditText fieldZipCode;
+    EditText fieldPassword;
+    EditText fieldConfirmPassword;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        fieldFName = (EditText)findViewById(R.id.fieldFName);
+        fieldLName = (EditText)findViewById(R.id.fieldLName);
+        fieldEmail = (EditText)findViewById(R.id.fieldEmail);
+        fieldPhone = (EditText)findViewById(R.id.fieldPhoneNumber);
+        fieldZipCode = (EditText)findViewById(R.id.fieldZipCode);
+        fieldPassword = (EditText)findViewById(R.id.fieldPassword);
+        fieldConfirmPassword = (EditText)findViewById(R.id.fieldConfirmPassword);
 
-        Button registerButton = (Button) findViewById(R.id.btnNext);
+
+        Button registerButton = (Button) findViewById(R.id.btnSubmit);
         assert registerButton != null;
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent doneIntent = new Intent(view.getContext(), LoginActivity.class);
-                startActivityForResult(doneIntent, 0);
-                landingPage.landingActivity.finish();
-                finish(); //to stop back button from going back to main menu
+                fName = fieldFName.getText().toString();
+                lName = fieldLName.getText().toString();
+                email = fieldEmail.getText().toString();
+
+                isEmailValid(email);
+
+                phoneNumber = fieldPhone.getText().toString();
+                zipCode = isValidZipCode(fieldZipCode.getText().toString());
+
+                password = fieldPassword.getText().toString();
+
+                isPasswordValid(password);
+
+                confirmPassword = fieldConfirmPassword.getText().toString();
+
+                System.out.println(fName + " " + lName + " | " + email + " | " + phoneNumber + " | " + zipCode + " | " + password + " | " + confirmPassword);
+
+//                Intent doneIntent = new Intent(view.getContext(), LoginActivity.class);
+//                startActivityForResult(doneIntent, 0);
+//                landingPage.landingActivity.finish();
+//                finish(); //to stop back button from going back to main menu
             }
         });
+
 
         CheckBox cbMath = (CheckBox) findViewById(R.id.checkBoxMath);
         assert cbMath != null;
@@ -159,6 +203,15 @@ public class RegisterActivity extends AppCompatActivity {
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > 4;
+    }
+
+    private int isValidZipCode(String zipCode){
+        if (zipCode.equals("")){
+            return 76201;
+        }
+        else{
+            return Integer.parseInt(zipCode);
+        }
     }
 
 
